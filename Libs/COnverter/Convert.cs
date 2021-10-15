b4afc2268c90a6a39ab597d0c143555c767ff89a
SmartMdn <92577847+SmartMdn@users.noreply.github.com>
2021-10-15 15:15:51 +0300

    Add files via upload

---
+++ Libs/COnverter/Convert.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+namespace COnverter
+{
+    public class Convert
+    {
+        private static readonly string bukv = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static string NewCh(string sym, string otk)
+        {
+            var s = "";
+            switch (otk)
+            {
+                case "to" when System.Convert.ToInt32(sym) > 10:
+                    s += bukv.Substring(System.Convert.ToInt32(sym) - 10, 1);
+                    break;
+                case "to":
+                case "from" when bukv.IndexOf(sym, StringComparison.Ordinal) == -1:
+                    s += sym;
+                    break;
+                case "from":
+                    s += (bukv.IndexOf(sym, StringComparison.Ordinal) + 10).ToString();
+                    break;
+            }
+
+            return s;
+        }
+
+        public static string ToN(string number, string sys)
+        {
+            var newNum = "";
+            var num = System.Convert.ToInt32(number);
+            var chast = System.Convert.ToInt32(number);
+            var numTemp = new ArrayList();
+            while (chast > 0)
+            {
+                chast = chast / System.Convert.ToInt32(sys);
+                numTemp.Add(num - chast * System.Convert.ToInt32(sys));
+                num = chast;
+            }
+
+            int j;
+            for (j = numTemp.Count - 1; j >= 0; j--)
+                newNum += NewCh(numTemp[j].ToString(), "to");
+            return newNum;
+        }
+
+
+        public static string FromN(string number, string sys)
+        {
+            var newNum = 0;
+            var temp = "";
+            int t;
+            int i;
+            for (i = 0; i < number.Length; i++)
+            {
+                temp = "";
+                temp += NewCh(number.Substring(i, 1), "from");
+                t = (int)Math.Pow(System.Convert.ToDouble(sys), System.Convert.ToDouble(number.Length - (i + 1)));
+                newNum += System.Convert.ToInt32(temp) * t;
+            }
+
+            return newNum.ToString();
+        }
+
+    }
+}
---
+++ Libs/Laba4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using COnverter;
+
+namespace Laba4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var tom = new COnverter.Convert();
+            var path = @"D:\SomeDir3";
+            var dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists) dirInfo.Create();
+            string b = "", a, flag = "0";
+
+            using (var fstream = new FileStream($@"{path}\note.txt", FileMode.OpenOrCreate))
+            {
+                // считываем первый символ
+                var output = new byte[1];
+                fstream.Read(output, 0, 1);
+                // декодируем байты в строку
+                flag = Encoding.Default.GetString(output);
+                output = new byte[fstream.Length - 1];
+                fstream.Read(output, 0, output.Length);
+                // декодируем байты в строку
+                a = Encoding.Default.GetString(output);
+                Console.WriteLine($"Текст из файла: {a}"); // hello house
+            }
+
+            switch (flag)
+            {
+                case "1":
+                    b = COnverter.Convert.FromN(a, "16");
+                    break;
+
+                case "2":
+
+                    b = COnverter.Convert.FromN(a,"8");
+                    break;
+
+                case "3":
+                    b = COnverter.Convert.FromN(a, "3");
+                    break;
+
+                case "4":
+                    b = COnverter.Convert.FromN(a, "2");
+                    break;
+            }
+
+            if (b != "")
+            {
+            }
+
+
+            try
+            {
+                if (args[0] == "-NoWindow")
+                {
+                    Console.WriteLine("Полученное значение: " + b);
+                    Console.Read();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            using (var fstream = new FileStream($"{path}\\result.txt", FileMode.Create))
+            {
+                var array = Encoding.Default.GetBytes(b);
+                // асинхронная запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+    }
+}
